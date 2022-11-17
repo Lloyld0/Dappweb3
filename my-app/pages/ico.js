04008d51d6a272537bf284af786f9442abe93b13
Lloyld0 <sebastien.mesguen@gmail.com>
2022-11-17 12:07:54 +0700

    App4

--- my-app/pages/ico.js
+++ my-app/pages/ico.js
@@ -404,7 +404,7 @@ return (
         )}
       </div>
       <div>
-        <img className={styles.image} src="./0.svg" />
+        <img className={styles.image} src="./cryptodevs/0.svg" />
       </div>
     </div>
 
