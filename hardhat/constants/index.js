a52812e5b44f0cf16bc42d688cc8f90d471ffde2
Lloyld0 <sebastien.mesguen@gmail.com>
2022-11-17 11:28:40 +0700

    First App

---
+++ hardhat/constants/index.js
@@ -0,0 +1,15 @@
+// IMPORT FROM NFT COLLECTION CONTRACT
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xD73F6579B3c2118234cB2271f5e2DeFB9adb09E0";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
+// IMPORT FROM ICO CONTRACT
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xf99B407242981CCc14Df1651457CebD9c2b987B1";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
+const CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS = "0xA84C243C03fdcBc5c595244e9bfac2Bcc9E44e0D";
