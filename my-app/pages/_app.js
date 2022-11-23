16930a1945b15229dc1a2ade758300e17f742342
Lloyld0 <sebastien.mesguen@gmail.com>
2022-11-23 18:40:27 +0700

    App

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,12 +1,7 @@
 import '../styles/globals.css'
-import { Provider } from "@self.id/react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Provider client={{ ceramic: "testnet-clay" }}>
-      <Component {...pageProps} />;
-    </Provider>
-  );
+  return <Component {...pageProps} />
 }
 
 export default MyApp
